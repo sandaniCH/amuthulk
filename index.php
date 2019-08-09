babb7e5e756961c4cfc5d85adaa1be4c60811639
sandaniCH <53854401+sandaniCH@users.noreply.github.com>
2019-08-09 08:57:12 +0530

    akila
    akila

---
+++ index.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Inedx page- Amuthu.lk</title>
+	<link rel="stylesheet" href="css/index.css" type="text/css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+	<!-- navigation bar -->
+	<div class="header">
+		<!-- Company name -->
+		<a href="index.php" class="logo">AMUTHU.LK</a>
+		<!-- header center content -->
+		<div class="header-center" class="dropdown">
+
+			<div class="dropdown">
+				<button class="dropbtn" class="active">Catogories
+					<i class="fa fa-caret-down"></i>
+				</button>
+				<div class="dropdown-content">
+					<a href="category.php?category=Vehicles">Vehicles</a>
+					<a href="category.php?category=Electronics">Electronics</a>
+					<a href="category.php?category=Properties">Properties</a>
+				</div>
+			</div>
+
+			<a href="shop.php">Shop</a>
+			<a href="contact us.php">Contact us</a>
+			<a href="About us.php">About us</a>
+		</div>
+		<!-- header right content  -->
+		<div class="header-right">
+
+			<a href="login/login.php">Sign in <i class="fa fa-chevron-circle-right"></i></a>
+			<a href=""><i class="fa fa-search"></i></a>
+		</div>
+
+	</div><!-- navigation bar div end -->
+
+	<table id="headert" style="width: 100%;height: auto;">
+		<tr>
+			<td class="table-head">
+
+				<div class="header-content">
+					<br><br><br><br><br><br><br><br><br>
+					<h1 class="header-text">ANYTHING</h1>
+					<h1 class="header-text">YOU</h1>
+					<h1 class="header-text">NEED</h1>
+
+					<h3>#FindYourDreams</h3>
+					<button class="head-btn">AMUTHU VEHICLES</button> <button class="head-btn">AMUTHU ELECTRONICS</button><br>
+					<button class="head-btnd">AMUTHU PROPERTIES</button>
+
+
+				</div>
+			</td>
+			<td class="table-head">
+				<img src="images/new/test1.png" style="width: 80%">
+			</td>
+		</tr>
+	</table>
+
+	<div id="section2">
+		<!-- suthura's shit -->
+	</div>
+
+	<table id="headert" style="width: 100%;height: auto;">
+		<tr>
+			<td class="table-head">
+				<img src="images/girlback.png" style="width: 80%">
+
+			</td>
+			<td class="table-head">
+
+				<div class="header-content">
+					<br><br><br><br><br><br><br><br><br>
+					<h4>#NEW SUMMER OFFERS 2019</h4>
+					<h1 class="header-text">DISCOUNTS</h1>
+					<h1 class="header-text">UPTO</h1>
+					<h1 class="header-text">50% ON</h1>
+					<h1 class="header-text">ELECTRONICS</h1>
+
+					<button class="head-btnd" style="background-color: black;">AMUTHU PROPERTIES</button>
+
+				</div>
+
+			</td>
+		</tr>
+	</table>
+
+	<div id="section3">
+		<center>
+			<h2 class="sec3-text">Collection</h2>
+		</center>
+	</div>
+
+	<table id="headert" style="width: 100%;height: auto;">
+		<tr>
+			<td class="table-head">
+				<img src="images/shopc.jpg" style="width: 80%">
+
+			</td>
+			<td class="table-head">
+
+				<div class="header-content">
+					<br><br><br><br><br><br><br><br><br>
+					<h4>#NEW SUMMER OFFERS 2019</h4>
+					<h1 class="header-text">DISCOUNTS</h1>
+					<h1 class="header-text">UPTO</h1>
+					<h1 class="header-text">50% ON</h1>
+					<h1 class="header-text">ELECTRONICS</h1>
+
+					<button class="head-btnd" style="background-color: black;">AMUTHU PROPERTIES</button>
+
+				</div>
+
+			</td>
+		</tr>
+	</table>
+
+	<footer>
+		<center>
+			<table style="width: 100%; text-align: center;">
+				<tr>
+					<td style="text-align: center;">
+						<h3>About Us</h3><br>
+						<h4>This is a project done by 18.2 Plymouth SE Group</h4>
+						<br><br>
+						<h3>Subscribe</h3><br>
+						<input type="text" placeholder="Search.." name="search">
+						<button type="submit"><i class="fa fa-search"></i></button>
+					</td>
+					<td>
+						<h3>Quick Links</h3><br>
+						<a href="index.php">Home</a><br>
+						<a href="shop.php">Shop</a><br>
+						<a href="ABout us.php">About US</a><br>
+						<br><br>
+
+					</td>
+					<td>
+						<h3>Contact Info</h3><br>
+						<h5><i class="fa fa-map-marker" style="font-size:24px"></i> 203 Mahenwaththa, Pitipana, Homagama</h5><br>
+						<h5><i class="fa fa-phone" style="font-size:24px"></i> +94 011 4576 964</h5><br>
+						<h5><i class="fa fa-vcard" style="font-size:24px"></i> amuthushoping@amuthu.com</h5><br>
+					</td>
+				</tr>
+			</table>
+		</center>
+	</footer>
+
+
+</body>
+
+</html>
